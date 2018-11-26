deaa513391f5d4de5281d7f74bc93027b6ce794c
André Leoni <andreluizleoni@gmail.com>
2018-11-26 17:53:07 -0200

    Send nil when value is not present and have on custom sanitizers

--- lib/attributes_sanitizer/predefined.rb
+++ lib/attributes_sanitizer/predefined.rb
@@ -1,20 +1,19 @@
 AttributesSanitizer.define_sanitizer :downcase do |value|
-  value.to_s.downcase
+  value.to_s.downcase if value
 end
 
 AttributesSanitizer.define_sanitizer :upcase do |value|
-  value.to_s.upcase
+  value.to_s.upcase if value
 end
 
 AttributesSanitizer.define_sanitizer :strip_tags do |value|
-  ActionController::Base.helpers.sanitize(value.to_s, tags: []).strip
+  ActionController::Base.helpers.sanitize(value.to_s, tags: []).strip if value
 end
 
 AttributesSanitizer.define_sanitizer :strip_emojis do |value|
-  value.to_s.gsub(AttributesSanitizer::EMOJI_REGEX, '')
+  value.to_s.gsub(AttributesSanitizer::EMOJI_REGEX, '') if value
 end
 
 AttributesSanitizer.define_sanitizer :strip_spaces do |value|
-  value.to_s.strip
+  value.to_s.strip if value
 end
-
