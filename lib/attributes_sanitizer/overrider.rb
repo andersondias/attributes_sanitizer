801efe7e64bd26b92f7b0a7550fe4241dc99c5af
Anderson Dias <andersondaraujo@gmail.com>
2018-11-27 11:08:10 -0200

    Reducing cyclomatic complexity for Overrider class
    Based on code climate issues I've refactored the class in order to reduce code duplication and complexity.

--- lib/attributes_sanitizer/overrider.rb
+++ lib/attributes_sanitizer/overrider.rb
@@ -9,29 +9,41 @@ module AttributesSanitizer
     def override_getters_and_setters
       return if klass.attributes_sanitize_map.blank?
 
-      klass.attributes_sanitize_map.each_pair do |attribute, _|
-        getter = attribute.to_sym
-        setter = :"#{attribute}="
-
-        unless klass.method_defined?(getter)
-          klass.define_method getter do
-            attribute_value = self[getter.to_s]
-            return if attribute_value.nil?
-
-            self.class.execute_sanitizers_for(attribute, attribute_value)
-          end
-        end
-
-        unless klass.method_defined?(setter)
-          klass.define_method(setter) do |new_value|
-            if new_value.present?
-              new_value = self.class.execute_sanitizers_for(attribute, new_value)
-            end
-
-            super(new_value)
-          end
-        end
+      attributes_to_override.each do |attribute|
+        override_getter(attribute)
+        override_setter(attribute)
       end
     end
+
+    private
+
+      def attributes_to_override
+        klass.attributes_sanitize_map.keys
+      end
+
+      def override_method(method_name, &block)
+        return if klass.method_defined?(method_name)
+        klass.define_method(method_name, &block)
+      end
+
+      def override_getter(attribute)
+        getter = attribute.to_sym
+        override_method(getter) do
+          attribute_value = self[getter.to_s]
+          return if attribute_value.nil?
+
+          self.class.execute_sanitizers_for(attribute, attribute_value)
+        end
+      end
+
+      def override_setter(attribute)
+        override_method(:"#{attribute}=") do |new_value|
+          if new_value.present?
+            new_value = self.class.execute_sanitizers_for(attribute, new_value)
+          end
+
+          super(new_value)
+        end
+      end
   end
 end
